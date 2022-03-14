3c5967ff9f7eedb8db287dc925b6430f032d829a
Kapilas Gnanarajah <kapilas.gnanarajah@stud.unibas.ch>
2022-03-14 17:35:44 +0100

    added findplane method

--- Assets/Scripts/VitrivrVR/Interaction/System/Grab/DrawingData.cs
+++ Assets/Scripts/VitrivrVR/Interaction/System/Grab/DrawingData.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Numerics;
+using MathNet.Numerics.LinearAlgebra;
 using UnityEngine;
 using Plane = UnityEngine.Plane;
 using Vector3 = UnityEngine.Vector3;
@@ -10,7 +11,9 @@ namespace VitrivrVR.Interaction.System.Grab
     {
         private List<LineRenderer> _drawnLines;
         private List<Vector3> _points; // contains from every line first, last and middle point OR random number of points
-        private Plane _plane;
+        private Vector3 _supportVector;
+        private Vector3 _normalVector;
+        public Plane plane;
         
         public DrawingData()
         {
@@ -22,7 +25,7 @@ namespace VitrivrVR.Interaction.System.Grab
             _drawnLines.Add(line);
         }
 
-        private void SetPoint()
+        private void SetPoints()
         {
             int numberOfPoints = 0;
             foreach (var line in _drawnLines)
@@ -42,6 +45,7 @@ namespace VitrivrVR.Interaction.System.Grab
                     _points.Add(line.GetPosition(0));
                     _points.Add(line.GetPosition(numberOfPoints / 2));
                     _points.Add(line.GetPosition(numberOfPoints - 1));
+                    // or add all points
                 }
             }
         }
@@ -57,14 +61,53 @@ namespace VitrivrVR.Interaction.System.Grab
             {
                 // TODO Regressionsebene durch Punktwolke
                 // https://www.matheboard.de/archive/550004/thread.html
-                _plane = new Plane();
+                FindPlane();
             }
         }
-        
-        
-        // TODO get random points from lines
-        // TODO getPlane create plane class
+
+        private void FindPlane()
+        {
+            // make a matrix out of all points which is a vector list
+            var A = Matrix<float>.Build;
+            int n = _points.Count; 
+            float[,] vectorArray = new float[n,3];
+            Vector3 center = new Vector3(0, 0, 0);
+            for (int i = 0; i < n; i++)
+            {
+                vectorArray[i, 0] = _points[i].x; 
+                vectorArray[i, 1] = _points[i].y; 
+                vectorArray[i, 2] = _points[i].z;
+                center.x += _points[i].x; 
+                center.y += _points[i].y; 
+                center.z += _points[i].z;
+            }
+            center /= n;
+            _supportVector = center;
+            var a = A.DenseOfArray(vectorArray);
+            var decomp = a.Svd(true);
+            var v = decomp.VT.Transpose(); 
+            // returns 3x3 matrix where the first 2 colums are "Richtungvektoren" and the 3. is normal vector to plane.
+            var normal = v.Column(2);
+            _normalVector = new Vector3(normal[0], normal[1], normal[2]);
+            plane.SetNormalAndPosition(_normalVector, _supportVector);
+        }
+
+        private Vector3 ProjectToPlane(Vector3 v)
+        {
+            Vector3 result;
+            var distance = plane.GetDistanceToPoint(v);
+            var factor = Vector3.Dot((v - _supportVector), _normalVector);
+            var div = Vector3.Dot(_normalVector, _normalVector);
+            result = v - (factor / div)*_normalVector; 
+            // TODO find better source 
+            // source:https://studyflix.de/mathematik/orthogonale-projektion-1468
+            
+            // TODO convert 3D vectors to 2D vectors
+            return result;
+        }
+        // TODO use GIZMOS to test what you wrote so far!!!
+
         // TODO get character recognition method
-        
+
     }
 }
